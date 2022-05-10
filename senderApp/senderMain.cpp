4e94e706b183b7c5474ec0b62ef2145c88c0c5f1
Gunaseelan <62167785+GunaseelanRajamanickam@users.noreply.github.com>
2022-05-10 13:35:59 +0530

    Update senderMain.cpp

--- senderApp/senderMain.cpp
+++ senderApp/senderMain.cpp
@@ -9,9 +9,10 @@ int main()
     std::string temperatureValueFilePath = "../temperatureRangeOfValues.txt";
     std::string socValueFilePath = "../SoCValuesFromSensor.txt";
     Sender senderObj;
-    senderObj.readFromFile(temperatureValueFilePath, "TEMPERATURE");
-    senderObj.readFromFile(socValueFilePath, "SOC");
-    senderObj.writeToConsole();
+    Sender* senderObj = new Sender;
+    senderObj->readFromFile(temperatureValueFilePath, "TEMPERATURE");
+    senderObj->readFromFile(socValueFilePath, "SOC");
+    senderObj->writeToConsole();
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
     return 0;
